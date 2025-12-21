518f91486b1557c2eace7839e44c702200d1227b
jks15063 <jks15063@gmail.com>
2025-12-21 13:01:12 -0700

    clean up main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use crate::camera::Camera;
 use core::f64;
 use hittable_list::HittableList;
-use ray::Ray;
 use sphere::Sphere;
 use vec3::Point3;
 
@@ -13,24 +12,6 @@ mod interval;
 mod ray;
 mod sphere;
 mod vec3;
-
-pub fn degrees_to_radians(degrees: f64) -> f64 {
-    degrees * f64::consts::PI / 180.0
-}
-
-pub fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
-    let oc = center - r.origin();
-    let a = r.direction().length_squared();
-    let h = vec3::dot(r.direction(), oc);
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = h * h - a * c;
-
-    if discriminant < 0.0 {
-        return -1.0;
-    } else {
-        return (h - discriminant.sqrt()) / a;
-    }
-}
 
 fn main() {
     // World
