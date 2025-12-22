7beffde4a867ac30b0d64f5c09d2d068cc54d0fa
jks15063 <jks15063@gmail.com>
2025-12-21 21:05:32 -0700

    Add fuzz

--- src/material.rs
+++ src/material.rs
@@ -32,19 +32,25 @@ impl Material for Lambertian {
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Self { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, r_in: Ray, rec: HitRecord) -> Option<(Color, Ray)> {
         let reflected = vec3::reflect(r_in.direction(), rec.normal);
+        let reflected = vec3::unit_vector(reflected) + (self.fuzz * vec3::random_unit_vector());
         let scattered = Ray::new(rec.p, reflected);
 
-        Some((self.albedo, scattered))
+        if vec3::dot(scattered.direction(), rec.normal) > 0.0 {
+            Some((self.albedo, scattered))
+        } else {
+            None
+        }
     }
 }
