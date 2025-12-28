4631b9aa8c10b154bcae5da61a0cad9d35f170bf
jks15063 <jks15063@gmail.com>
2025-12-28 15:49:26 -0700

    Add missing reflectance check & regen final image

--- src/material.rs
+++ src/material.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use crate::color::Color;
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
@@ -79,7 +81,7 @@ impl Material for Dielectric {
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
         let cannot_refract = ri * sin_theta > 1.0;
 
-        let direction = if cannot_refract {
+        let direction = if cannot_refract || reflectance(cos_theta, ri) > rand::rng().random() {
             vec3::reflect(unit_direction, rec.normal)
         } else {
             vec3::refract(unit_direction, rec.normal, ri)
@@ -93,7 +95,6 @@ impl Material for Dielectric {
 
 fn reflectance(cosine: f64, refraction_index: f64) -> f64 {
     let r0 = (1.0 - refraction_index) / (1.0 + refraction_index).powi(2);
-    // let r0 = r0 * r0;
 
     r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
 }
