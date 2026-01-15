876c50cebd7c17c2b8e572656a3a2dd2e6ee19da
jks15063 <jks15063@gmail.com>
2026-01-14 22:41:18 -0700

    bug fix

--- src/hittable.rs
+++ src/hittable.rs
@@ -91,8 +91,8 @@ impl RotateY {
             for j in 0..2 {
                 for k in 0..2 {
                     let x = i as f64 * bbox.x.max + (1 - i) as f64 * bbox.x.min;
-                    let y = i as f64 * bbox.y.max + (1 - j) as f64 * bbox.y.min;
-                    let z = i as f64 * bbox.z.max + (1 - k) as f64 * bbox.z.min;
+                    let y = j as f64 * bbox.y.max + (1 - j) as f64 * bbox.y.min;
+                    let z = k as f64 * bbox.z.max + (1 - k) as f64 * bbox.z.min;
 
                     let newx = cos_theta * x + sin_theta * z;
                     let newz = -sin_theta * x + cos_theta * z;
